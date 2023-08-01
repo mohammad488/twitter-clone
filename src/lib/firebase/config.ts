fd17788853ba5233b5a3fd383c5e5063dddfd5ee
Mohammad Hajikandi <73636328+mohammad488@users.noreply.github.com>
2023-08-01 20:58:45 +0330

    Update config.ts

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: "AIzaSyAE3kjlepMdiInFjPr0oBjJfQC0b5Ietbs",
-  authDomain: "strings-a2531.firebaseapp.com",
-  projectId: "strings-a2531",
-  storageBucket: "strings-a2531.appspot.com",
-  messagingSenderId: "143251220617",
-  appId: "1:143251220617:web:7bd5e2d365af10d524fe58",
-  measurementId: "G-4EGSL0T20M"
+  apiKey: 'AIzaSyAE3kjlepMdiInFjPr0oBjJfQC0b5Ietbs',
+  authDomain: 'strings-a2531.firebaseapp.com',
+  projectId: 'strings-a2531',
+  storageBucket: 'strings-a2531.appspot.com',
+  messagingSenderId: '143251220617',
+  appId: '1:143251220617:web:7bd5e2d365af10d524fe58',
+  measurementId: 'G-4EGSL0T20M'
 } as const;
 
 type Config = typeof config;
